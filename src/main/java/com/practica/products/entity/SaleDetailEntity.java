556d6c5e9357d87b06e7c0ef04b14505ebad62c3
david26044 <davidcarreno767@gmail.com>
2025-05-20 09:44:37 -0500

    Pequeño cambio en SaleDetailEntity

--- src/main/java/com/practica/products/entity/SaleDetailEntity.java
+++ src/main/java/com/practica/products/entity/SaleDetailEntity.java
@@ -15,12 +15,12 @@ public class SaleDetailEntity implements Serializable {
 
     @ManyToOne(fetch = FetchType.EAGER)
     @MapsId("productId")
-    @JoinColumn(name = "product_id", insertable = false, updatable = false)
+    @JoinColumn(name = "product_id")
     private ProductEntity product;
 
     @ManyToOne(fetch = FetchType.EAGER)
     @MapsId("saleId")
-    @JoinColumn(name = "sale_id", insertable = false, updatable = false)
+    @JoinColumn(name = "sale_id")
     @JsonBackReference
     private SaleEntity sale;
 
